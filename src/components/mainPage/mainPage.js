ea6801bb6e27e9405666f18486f460c19f5d2ea0
Alexandr <sashka.gm@gmail.com>
2022-05-20 10:46:25 +0600

    merge

--- src/components/mainPage/mainPage.js
+++ src/components/mainPage/mainPage.js
@@ -1,7 +1,20 @@
-
+import { useEffect } from "react";
+import useConnectServer from "../../services/connect/connect";
 
 const MainPage=()=>{
+    const {error, postData}=useConnectServer();
 
+    useEffect(()=>{
+        postData({testData:{btn1:22, btn2:333}})
+                .then(data=>{
+                    console.log(data);
+                })
+                .catch((e)=>{
+                    console.log(`state error: ${error}`);
+                    console.log(`request error: ${e}`);
+                })
+                // eslint-disable-next-line
+    },[])
     return (
         <div className="main-page">
             <h3>Основная страница</h3>
